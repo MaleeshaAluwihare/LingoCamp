59197475f1c82298538436652b9e0e24cb9988e7
MaleeshaAluwihare <159033744+MaleeshaAluwihare@users.noreply.github.com>
2025-03-20 00:08:04 +0530

    Update FirebaseConfig.java

--- backend/lingocamp/src/main/java/com/skillshareplatform/lingocamp/config/FirebaseConfig.java
+++ backend/lingocamp/src/main/java/com/skillshareplatform/lingocamp/config/FirebaseConfig.java
@@ -1,5 +1,25 @@
 package com.skillshareplatform.lingocamp.config;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class FirebaseConfig {
-    
+
+    public void initializeFirebase() throws IOException {
+
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
+
+        // Initialize Firebase with the service account credentials
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        // Initialize the default FirebaseApp instance
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+    }
 }
