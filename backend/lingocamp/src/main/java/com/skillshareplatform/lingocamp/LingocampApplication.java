a821bbb2c86620530dd088bac316375a367482d2
MaleeshaAluwihare <159033744+MaleeshaAluwihare@users.noreply.github.com>
2025-03-19 12:52:38 +0530

    initial commit

---
+++ backend/lingocamp/src/main/java/com/skillshareplatform/lingocamp/LingocampApplication.java
@@ -0,0 +1,13 @@
+package com.skillshareplatform.lingocamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingocampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingocampApplication.class, args);
+	}
+
+}
