fc960dc503d5e9947c76029b58ec6e9ffa9f0757
MaleeshaAluwihare <159033744+MaleeshaAluwihare@users.noreply.github.com>
2025-03-28 23:20:31 +0530

    implement delete profile endpoint

--- backend/lingocamp/src/main/java/com/skillshareplatform/lingocamp/controller/TutorManagement/TutorController.java
+++ backend/lingocamp/src/main/java/com/skillshareplatform/lingocamp/controller/TutorManagement/TutorController.java
@@ -5,6 +5,8 @@ import java.util.concurrent.ExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.encrypt.RsaAlgorithm;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QuerySnapshot;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.skillshareplatform.lingocamp.model.TutorManagement.TutorModel;
 import com.skillshareplatform.lingocamp.service.TutorManagement.TutorService;
 
@@ -99,5 +102,21 @@ public class TutorController {
         } catch (InterruptedException | ExecutionException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error updating profile");
         }
+    }
+
+    // Delete existing profile
+    @DeleteMapping("deleteprofile/{uid}")
+    public ResponseEntity<String> deleteTutorProfile(@PathVariable String uid) {
+        try {
+            tutorService.deleteTutorProfile(uid);
+            return ResponseEntity.ok("Profile deleted successfully");
+        } catch (FirebaseAuthException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not in authentication system");
+        }catch (InterruptedException | ExecutionException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Deletion failed");
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+
     }
 }
