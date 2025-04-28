a367dae958226e46cb6a647fe1f56889e1246675
MaleeshaAluwihare <159033744+MaleeshaAluwihare@users.noreply.github.com>
2025-04-28 08:28:01 +0530

    Resolve the conflicts

---
+++ backend/lingocamp/src/main/java/com/skillshareplatform/lingocamp/repository/CourseManagement/CourseRepository.java
@@ -0,0 +1,33 @@
+package com.skillshareplatform.lingocamp.repository.CourseManagement;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
+import com.skillshareplatform.lingocamp.model.TutorManagement.CourseModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.ExecutionException;
+
+@Repository
+public class CourseRepository {
+
+    @Autowired
+    private Firestore firestore;
+
+    public boolean doesTutorExist(String tutorId) throws ExecutionException, InterruptedException {
+        DocumentReference tutorRef = firestore.collection("Tutors").document(tutorId);
+        DocumentSnapshot tutorSnapshot = tutorRef.get().get();
+        return tutorSnapshot.exists();
+    }
+
+    public CourseModel saveCourse(CourseModel course) throws ExecutionException, InterruptedException {
+        DocumentReference docRef = firestore.collection("Courses").document();
+        
+        course.setCourseId(docRef.getId());
+        
+        ApiFuture<WriteResult> future = docRef.set(course);
+        future.get(); 
+        
+        return course;
+    }
+}
